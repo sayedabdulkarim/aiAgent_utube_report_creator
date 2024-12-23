ae6d6d089868b65af6b6c9b9947eb7712b1f6a91
Sayed Abdul Karim <43297672+sayedabdulkarim@users.noreply.github.com>
2024-12-24 03:24:02 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,7 +1,7 @@
 # from crewai_tools import YoutubeChannelSearchTool
 
 # Initialize the tool with a specific YouTube channel handle to target your search
-# yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@krishnaik06')
+# yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@moshhamedani')
 
 # ## https://serper.dev/
 
